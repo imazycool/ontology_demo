d002bbc10cbcbeb17694a558152b6a56c06977d6
imazy <imazycool@gmail.com>
2026-06-28 18:35:06 +0530

    first skelton created

---
+++ src/services/metadata_services.py
@@ -0,0 +1,10 @@
+class MetadataService:
+
+    def get_entity(self, entity_name):
+        pass
+
+    def get_attribute(self, attribute_name):
+        pass
+
+    def get_metric(self, metric_name):
+        pass
